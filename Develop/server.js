62df75516d299ec1a7927b7b00223484fb0df9c0
caoimhejyoti <caoimhejyoti@gmail.com>
2023-02-06 14:49:41 +0800

    saving to db.json working.

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const notesDb = require('./db/db.json');
+const fs = require('fs');
 const PORT = 3001;
 
 const app = express();
@@ -11,6 +11,7 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
+// HTML ROUTES --------------------------------
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -19,9 +20,67 @@ app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// app.get('/notes', (req, res) =>  {
-//     res.json(notesDb)
-// });
+// API ROUTES --------------------------------
+
+// GET request for notes
+app.get('/api/notes', (req, res) =>  {
+  // Send a message to the client
+  res.json(`${req.method} request received to get notes`);
+
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get notes - console req`);
+});
+
+// POST request to save a note
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+
+  // Destructuring assignment for the items in req.body
+  const { title, text } = req.body;
+
+  // If all the required properties are present
+  if (title && text) {
+    // Variable for the object we will save
+    const newNote = {
+      title,
+      text
+    };
+
+    // Obtain existing notes
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+        // Convert string into JSON object
+        const parsedNotes = JSON.parse(data);
+
+        // Add new note
+        parsedNotes.push(newNote);
+
+        // Write updated notes back to the file
+        fs.writeFile(
+          './db/db.json',
+          JSON.stringify(parsedNotes, null, 4),
+          (writeErr) =>
+            writeErr
+              ? console.error(writeErr)
+              : console.info('Successfully saved note!')
+        );
+      }
+    });
+
+    const response = {
+      status: 'success',
+      body: newNote,
+    };
+
+    console.log(response);
+    res.status(201).json(response);
+  } else {
+    res.status(500).json('Error in saving note');
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
