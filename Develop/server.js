981d16fe3ed7a6c8bd8193cffc9fd6b928ced54e
caoimhejyoti <caoimhejyoti@gmail.com>
2023-02-07 14:52:06 +0800

    routing api within seperate js files.

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const api = require('./routes/index');
 
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
@@ -12,6 +13,7 @@ const app = express();
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use('/api', api);
 
 app.use(express.static('public'));
 
@@ -20,82 +22,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/api/notes', (req, res) => {
+app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
-// API ROUTES --------------------------------
-
-// GET request for notes
-app.get('/api/notes', (req, res) =>  {
-  // Send a message to the client
-  res.json(`${req.method} request received to get notes`);
-
-  // Log our request to the terminal
-  console.info(`${req.method} request received to get notes - console req`);
-});
-
-// POST request to save a note
-app.post('/api/notes', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a note`);
-
-  // Destructuring assignment for the items in req.body
-  const { title, text } = req.body;
-
-  // If all the required properties are present
-  if (title && text) {
-    // Variable for the object we will save
-    const newNote = {
-      title,
-      text,
-      note_id: uuid()
-    };
-
-    // Obtain existing notes
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        // Convert string into JSON object
-        const parsedNotes = JSON.parse(data);
-
-        // Add new note
-        parsedNotes.push(newNote);
-
-        // Write updated notes back to the file
-        fs.writeFile(
-          './db/db.json',
-          JSON.stringify(parsedNotes, null, 4),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully saved note!')
-        );
-      }
-    });
-
-    const response = {
-      status: 'success',
-      body: newNote,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('Error in saving note');
-  }
-});
-
-// DELET request to save a note
-app.delete('/api/notes/:id', (req, res) => {
-  // Log that a DELETE request was received
-  console.info(`${req.method} request received to DELETE a note`);
-
-  res.send('DELETE request to notes')
-
-});
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
