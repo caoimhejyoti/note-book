042359ad28e5d3b9636a25410cfd54e5cd17926a
caoimhejyoti <caoimhejyoti@gmail.com>
2023-02-09 19:37:09 +0800

    testing with external testing

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -114,13 +114,13 @@ const handleNewNoteView = (e) => {
 };
 
 const handleRenderSaveBtn = () => {
-  // if (noteTitle.value.trim() || noteText.value.trim()) {
-  //   hide(saveNoteBtn);
-  //   console.log("in renderSaveBtn hide");
-  // } else {
-  //   show(saveNoteBtn);
-  // }
-  show(saveNoteBtn);
+  if (noteTitle.value.trim() || noteText.value.trim()) {
+    hide(saveNoteBtn);
+    console.log("in renderSaveBtn hide");
+  } else {
+    show(saveNoteBtn);
+  }
+  // show(saveNoteBtn);
 };
 
 // Render the list of note titles
