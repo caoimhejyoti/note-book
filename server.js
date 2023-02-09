fc883c26da517c31a3a59652b82250d9ea6c06cf
caoimhejyoti <caoimhejyoti@gmail.com>
2023-02-09 10:36:44 +0800

    final notes and tidy up beofer submission

--- server.js
+++ server.js
@@ -6,18 +6,19 @@ const api = require('./routes/index');
 // Helper method for generating unique ids
 const uuid = require('./helpers/uuid');
 
+//port used to connect to server
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Sets up the Express app to handle data parsing
+// Express middleware - Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api', api);
 
 app.use(express.static('public'));
 
-// HTML ROUTES --------------------------------
+// HTML ROUTES 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
